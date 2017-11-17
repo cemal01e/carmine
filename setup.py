89423b2737dbb64ff4ad734400811bbb52c75d88
Charles Newey <charlie.newey@flightdataservices.com>
2017-11-17 14:59:06 +0000

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+"""
+Carmine is a Class Association Rule discovery framework, aiming to implement
+various fast mining algorithms in Python.
+"""
+
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, "README.md"), encoding="utf-8") as f:
+    long_description = f.read()
+
+with open("requirements.txt", "r") as reqs:
+    install_requires = [l.rstrip() for l in reqs.readlines()]
+
+
+setup(
+    name="carmine",
+
+    version="0.1.0",
+
+    description="Association rule mining algorithms for Numpy and Pandas",
+    long_description=long_description,
+
+    url="https://github.com/pypa/sampleproject",
+
+    author="Charles Newey",
+    author_email="charlie.newey@flightdataservices.com",
+
+    license="BSD 3-Clause",
+
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+
+        "Intended Audience :: Developers",
+
+        "License :: OSI Approved :: BSD 3-Clause License",
+
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+    ],
+
+    keywords="machine learning association rule mining market basket analysis",
+
+    packages=find_packages(exclude=["contrib", "docs", "tests"]),
+
+    install_requires=install_requires
+)
