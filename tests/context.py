82082bfd56dcdc1a6fbe0b8032d78b82ebcc8317
Eron Cemal <eron.cemal@flightdataservices.com>
2018-04-03 16:55:23 +0100

    fix tests

--- tests/context.py
+++ tests/context.py
@@ -11,16 +11,19 @@ import carmine  # NOQA
 # verification dataset from paper below
 # DOI: http://dx.doi.org/10.1016/j.eswa.2012.10.035
 dataset = np.array([
-    [1, 1, 1, 1, 0],
-    [1, 2, 1, 0, 0],
-    [2, 2, 1, 0, 1],
-    [3, 3, 1, 1, 1],
-    [3, 1, 2, 0, 1],
-    [3, 3, 1, 1, 0],
-    [1, 3, 2, 1, 1],
-    [2, 2, 2, 0, 0]
+    [1, 1, 1, 1],
+    [1, 2, 1, 0],
+    [2, 2, 1, 0],
+    [3, 3, 1, 1],
+    [3, 1, 2, 0],
+    [3, 3, 1, 1],
+    [1, 3, 2, 1],
+    [2, 2, 2, 0]
 ])
 
+y2 = np.array([0, 0, 1, 0, 1, 1, 0, 1])
+
+dataset2 = np.hstack((dataset, y2[:,None]))
 X = dataset[:, :-1]  # attributes / features
-y = dataset[:, -2]  # class labels
-Y = dataset[:, -2:]  #  second class labels
+y = dataset[:, -1]  # class labels
+Y = dataset2[:, -2:]  #  second class labels
